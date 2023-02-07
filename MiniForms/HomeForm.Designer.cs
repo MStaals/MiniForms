187c0a0349bcc5fc5ca453e39e6003ab0d59fc49
Max Staals <mst@quadira.com>
2023-02-07 09:39:33 +0100

    MiniForms Begin

---
+++ MiniForms/HomeForm.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace MiniForms
+{
+    partial class HomeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtbModule = new System.Windows.Forms.RichTextBox();
+            this.btnExecute = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // rtbModule
+            // 
+            this.rtbModule.Location = new System.Drawing.Point(12, 12);
+            this.rtbModule.Name = "rtbModule";
+            this.rtbModule.Size = new System.Drawing.Size(188, 288);
+            this.rtbModule.TabIndex = 0;
+            this.rtbModule.Text = "FolderIn\nFolderout\nTextReplace\nMailOut\nConvert\nDecode\nToPDF\nWordTemplate";
+            this.rtbModule.DoubleClick += new System.EventHandler(this.rtbModule_DoubleClick);
+            // 
+            // btnExecute
+            // 
+            this.btnExecute.Location = new System.Drawing.Point(661, 392);
+            this.btnExecute.Name = "btnExecute";
+            this.btnExecute.Size = new System.Drawing.Size(94, 29);
+            this.btnExecute.TabIndex = 1;
+            this.btnExecute.Text = "Execute";
+            this.btnExecute.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(399, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(209, 288);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // HomeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btnExecute);
+            this.Controls.Add(this.rtbModule);
+            this.Name = "HomeForm";
+            this.Text = "HomeForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private RichTextBox rtbModule;
+        private Button btnExecute;
+        private RichTextBox richTextBox1;
+    }
+}
---
+++ MiniForms/HomeForm.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace MiniForms
+{
+    public partial class HomeForm : Form
+    {
+        public HomeForm()
+        {
+            InitializeComponent();
+        }
+
+        private void rtbModule_DoubleClick(object sender, EventArgs e)
+        {
+            
+
+            if (rtbModule.SelectedText == "FolderIn")
+            {
+
+                OpenFileDialog dlg = new OpenFileDialog();
+                dlg.ShowDialog();
+            }
+            else if(rtbModule.SelectedText == "FolderOut")
+            {
+                OpenFileDialog dlg = new OpenFileDialog();
+                dlg.ShowDialog();
+            }
+            else if(rtbModule.SelectedText == "")
+            {
+
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
